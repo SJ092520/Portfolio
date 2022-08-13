7379e3da854b37beae7171d3a03256ca344fb8e8
Shreinik Jain <1029384756hhh@gmail.com>
2022-08-13 21:56:22 +0530

    Next

--- question.js
+++ question.js
@@ -4,7 +4,7 @@ setTimeout(() => {
 }, 3000)
 
 setTimeout(() => {
-    document.getElementById('question').innerHTML = '<div class="card"><img src="vite.svg" alt="Avatar" style="width:100vw;height:30vh"><div class="container"><h2><b>Enter Your Name</b></h2><input type="text" name="name" id="name" class="input" placeholder="Your name here"/><br><button onclick="startGame(`Player`,`blue`)">Select Blue</button><button onclick="startGame(`Player`,`red`)">Select Red</button></div></div>';
+    document.getElementById('question').innerHTML = '<div class="card"><img src="vite.svg" alt="Avatar" style="width:100vw;height:30vh"><div class="container"><h2><b>Enter Your Name</b></h2><input type="text" name="name" id="name" class="input" placeholder="Your name here"/><br><button onclick="startGame(`Player`,`blue`)" class="blue-btn">Select Blue</button><button onclick="startGame(`Player`,`red`)" class="red-btn">Select Red</button></div></div>';
 }, 6000)
 
 
